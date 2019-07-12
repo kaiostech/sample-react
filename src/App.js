b81c87b4d8715228829c06b944b8093b746835fd
Johnatan Dias <johnatan.dias@kaiostech.com>
2019-07-11 22:18:55 -0300

    Add database service

--- src/App.js
+++ src/App.js
@@ -1,43 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Header, Input, ToDos, Softkey } from "./components";
 import { useNavigation } from "./hooks";
+import { DatabaseService } from './services/Database';
 
 export default function App() {
+  const getTodosFromDatabase = () => {
+    DatabaseService.init().then(async () => {
+      const todos = await DatabaseService.getAll();
+      setToDo(todos);
+    });
+  };
+
+  useEffect(getTodosFromDatabase, []);
+
   const [toDos, setToDo] = useState([]);
 
   const [current, setNavigation] = useNavigation();
 
-  const onKeyCenter = () => {
+  const onKeyCenter = async () => {
     const currentElement = document.querySelector("[nav-selected=true]");
     const currentNavigationIndex = parseInt(currentElement.getAttribute("nav-index"), 10);
 
     const isATask = currentNavigationIndex > 0;
     if (isATask) {
-      setToDo(prevState => {
-        const current = [...prevState];
-        current[currentNavigationIndex - 1].completed = !current[currentNavigationIndex - 1].completed;
-        return current;
-      });
+      const todoId = parseInt(currentElement.getAttribute("todo-id"), 10);
+      const allTodos = await DatabaseService.toggleCompleted(todoId);
+      setToDo(allTodos);
     } else if (currentElement.value.length) {
-      const toDo = { name: currentElement.value, completed: false };
-      setToDo(prevState => [...prevState, toDo]);
+      const allTodos = await DatabaseService.add(currentElement.value);
+      setToDo(allTodos);
       currentElement.value = "";
     }
   };
 
-  const onKeyRight = () => {
-    const currentIndex = parseInt(
-      document.querySelector("[nav-selected=true]").getAttribute("nav-index"),
-      10
-    );
+  const onKeyRight = async () => {
+    const currentElement = document.querySelector("[nav-selected=true]");
+    const currentIndex = parseInt(currentElement.getAttribute("nav-index"), 10);
+
     if (currentIndex > 0) {
-      setToDo(prevState => {
-        const current = [...prevState];
-        current.splice(currentIndex - 1, 1);
-        const goToPreviousElement = Boolean(current.length);
-        setNavigation(goToPreviousElement ? currentIndex - 1 : 0);
-        return current;
-      });
+      const todoId = parseInt(currentElement.getAttribute("todo-id"), 10);
+      const allTodos = await DatabaseService.deleteById(todoId);
+      const goToPreviousElement = Boolean(allTodos.length);
+      setNavigation(goToPreviousElement ? currentIndex - 1 : 0);
+      setToDo(allTodos);
     }
   };
 
---
+++ src/services/Database.js
@@ -0,0 +1,142 @@
+class DatabaseProvider {
+  constructor() {
+    this.db = {};
+    this.dbName = 'todos';
+    this.dbVersion = 1;
+    this.stores = { TODOS: 'todos' };
+    this.request = null;
+  }
+
+  init = () => {
+    return new Promise((resolve) => {
+      this.request = window.indexedDB.open(this.dbName, this.dbVersion);
+
+      this.request.onupgradeneeded = () => {
+        this.db = this.request.result;
+        this.createTodosStore();
+      };
+
+      this.request.onsuccess = event => {
+        this.db = event.target.result;
+        resolve();
+      };
+
+      this.request.onerror = error => console.error({ error });
+    });
+  }
+
+  createTodosStore = () => {
+    const storeConfig = { keyPath: "id", autoIncrement: true };
+    const objectStore = this.db.createObjectStore(this.stores.TODOS, storeConfig);
+    objectStore.createIndex("name", "name");
+    objectStore.createIndex("completed", "completed");
+  }
+
+  getTodosObjectStore = () => {
+    const transaction = this.db.transaction([this.stores.TODOS], "readwrite");
+    transaction.onerror = error => console.error('There was a transaction error', { error });
+    return transaction.objectStore(this.stores.TODOS)
+  }
+
+  add = name => {
+    return new Promise((resolve, reject) => {
+      const objectStore = this.getTodosObjectStore();
+      const addRequest = objectStore.put({ name, completed: false });
+      addRequest.onsuccess = async () => {
+        const allTodos = await this.getAll();
+        resolve(allTodos);
+      }
+      addRequest.onerror = error => reject(error);
+    });
+  };
+
+  updateById = (id, newValue) => {
+    return new Promise((resolve, reject) => {
+      const objectStore = this.getTodosObjectStore();
+      const keyRange = IDBKeyRange.only(id);
+      const cursor = objectStore.openCursor(keyRange);
+
+      cursor.onsuccess = () => {
+        const cursorWithValue = cursor.result;
+        const { value } = cursorWithValue;
+
+        const updateTo = { ...value, ...newValue };
+        const update = cursorWithValue.update(updateTo);
+        update.onsuccess = async () => {
+          const allTodos = this.getAll();
+          resolve(allTodos);
+        }
+        update.onerror = error => reject(error);
+      };
+
+      cursor.onerror = error => console.log('Item on check error', { error });
+    });
+  };
+
+  toggleCompleted = id => {
+    return new Promise((resolve, reject) => {
+      const objectStore = this.getTodosObjectStore();
+      const keyRange = IDBKeyRange.only(id);
+      const cursor = objectStore.openCursor(keyRange);
+
+      cursor.onsuccess = () => {
+        const cursorWithValue = cursor.result;
+        const { value } = cursorWithValue;
+
+        const updateTo = { ...value, completed: !value.completed };
+        const update = cursorWithValue.update(updateTo);
+        update.onsuccess = async () => {
+          const allTodos = await this.getAll();
+          resolve(allTodos);
+        }
+        update.onerror = err => reject(err);
+      };
+
+      cursor.onerror = error => console.log('Item on check error', { error });
+    });
+  }
+
+  deleteById = id => {
+    return new Promise((resolve, reject) => {
+      const objectStore = this.getTodosObjectStore();
+      const deleting = objectStore.delete(id);
+      deleting.onsuccess = async response => {
+        const allTodos = await this.getAll();
+        resolve(allTodos);
+      }
+      deleting.onerror = error => reject(error);
+    });
+  };
+
+  getAll = () => {
+    return new Promise((resolve, reject) => {
+      const objectStore = this.getTodosObjectStore();
+
+      const openCursor = objectStore.openCursor();
+      const items = [];
+
+      openCursor.onsuccess = event => {
+        const cursor = event.target.result;
+        if (cursor) {
+          items.push(cursor.value);
+          cursor.continue();
+        } else {
+          resolve(items);
+        }
+      };
+
+      openCursor.onerror = error => reject(error);
+    });
+  }
+
+  getById = id => {
+    return new Promise((resolve, reject) => {
+      const objectStore = this.getTodosObjectStore();
+      const request = objectStore.get(id);
+      request.onsuccess = () => request.result ? resolve(request.result) : reject("Not found");
+      request.onerror = error => reject(error);
+    });
+  };
+}
+
+export const DatabaseService = new DatabaseProvider();
