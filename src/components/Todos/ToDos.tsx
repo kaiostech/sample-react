ceb76c8a4000a2e16e6c0ced4ddf491c5bacbc41
Urhengulas <johann.hemmann@code.berlin>
2020-03-17 21:58:28 +0100

    Update components/ToDos to tsx

---
+++ src/components/Todos/ToDos.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import css from './ToDos.module.css';
+
+interface ToDosProps {
+	toDos: Array<{ completed: boolean, id: number, name: string; }>;
+}
+
+export const ToDos: React.FC<ToDosProps> = ({ toDos }) => {
+	if (toDos === undefined || !toDos.length) return null;
+
+	return (
+		<div className={css.todos}>
+			{
+				toDos.map((toDo, index) => (
+					<span
+						nav-selectable="true"
+						todo-id={toDo.id}
+						key={index}
+						className={`${css.todo} ${toDo.completed ? css.completed : ''}`}
+					>
+						{toDo.name}
+					</span>
+				))
+			}
+		</div>
+	);
+};
